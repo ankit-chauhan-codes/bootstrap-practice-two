6ded936b42a3cc149b703f4df941ceb2468ae946
webmark-ank <ankit.chauhan@bhadula.com>
2022-03-29 11:49:22 +0530

    active navbar styling in js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration){
         let obj = document.getElementById(id),
         current = start,
-        range = end-start,
+        range = end - start,
         increment = end > start ? 1 : -1,
         step = Math.abs(Math.floor(duration / range)),
         timer = setInterval(() => {
@@ -21,3 +21,20 @@ document.addEventListener("DOMContentLoaded", () => {
     counter("count3", 0, 1440, 3000);
     counter("count4", 0, 7110, 3000);
 });
+//counter-design//
+
+
+
+//active navbar//
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on")
+    }else{
+        nav.classList.remove("scroll-on")
+    }
+}
+//active navbar//
+
+
