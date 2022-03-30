eb604b8c56002ea74305657ee2daba304fc28949
webmark-ank <ankit.chauhan@bhadula.com>
2022-03-30 16:09:58 +0530

    corrections at js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 
-//active navbar//
+// navbar styling//
 
 let nav = document.querySelector(".navigation-wrap");
 window.onscroll = function () {
     if(document.documentElement.scrollTop > 20){
-        nav.classList.add("scroll-on")
+        nav.classList.add("scroll-on");
     }else{
-        nav.classList.remove("scroll-on")
+        nav.classList.remove("scroll-on");
     }
 }
-//active navbar//
+// navbar styling//
 
 
 
@@ -18,7 +18,7 @@ window.onscroll = function () {
 
 let navBar = document.querySelectorAll('.nav-link');
 let navCollapse = document.querySelector('.navbar-collapse.collapse');
-navBar.forEach(fuction(a){
+navBar.forEach(function(a){
     a.addEventListener("click", function(){
         navCollapse.classList.remove("show");
     })
